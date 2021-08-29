bcdd0523ac7a8f9973f419ef1b90b28231c0ec22
mad-skull <da.madskull@gmail.com>
2021-08-29 21:23:54 +0530

    share button function

--- app/src/main/java/com/example/meme_app/MainActivity.kt
+++ app/src/main/java/com/example/meme_app/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.meme_app
 
+import android.content.Intent
 import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -17,6 +18,9 @@ import com.bumptech.glide.request.target.Target
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var currentImageUrl: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -34,8 +38,8 @@ class MainActivity : AppCompatActivity() {
             url,
             null
                     Response . Listener { response ->
-                val url = response.getString( name: "url")
-                Glide.with(this).load(url).listener(object: RequestListener<Drawable>{
+                currentImageUrl = response.getString( name: "url")
+                Glide.with(this).load(currentImageUrl).listener(object: RequestListener<Drawable>{
 
                     override fun onLoadFailed(
                         e: GlideException?,
@@ -74,7 +78,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun shareMeme(view: android.view.View) {
-
+        val intent  = Intent(Intent.ACTION_SEND)
+        intent.putExtra(Intent.EXTRA_TEXT, "Hey Check this cool meme from Reddit $currentImageUrl")
+        val chooser = Intent.createChooser(intent, "Share this meme using...")
+        startActivity(chooser)
     }
     fun nextMeme(view: android.view.View) {
         loadMeme()
