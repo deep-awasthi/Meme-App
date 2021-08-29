0cbbaf59f53efe8c63a291a53490a8e5447a4730
mad-skull <da.madskull@gmail.com>
2021-08-29 20:33:04 +0530

    adding volly library for random meme api

--- app/src/main/java/com/example/meme_app/MainActivity.kt
+++ app/src/main/java/com/example/meme_app/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.meme_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,6 +13,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun shareMeme(view: android.view.View) {}
-    fun nextMeme(view: android.view.View) {}
+    private fun loadMeme() {
+
+        // Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://meme-api.herokuapp.com/gimme"
+
+        // Request a string response from the provided URL.
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            Response.Listener<String> { response ->
+
+            },
+            Response.ErrorListener {
+
+            })
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest)
+
+
+    }
+
+    fun shareMeme(view: android.view.View) {
+
+    }
+    fun nextMeme(view: android.view.View) {
+
+    }
 }
